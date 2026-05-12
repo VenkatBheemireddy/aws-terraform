89131415f1171ea2bacfe23fb9821885505fd5a8
venkat <devops.bheemireddy@gmail.com>
2026-05-13 00:34:06 +1000

    Terraform commit

--- 09-modules/main.tf
+++ 09-modules/main.tf
@@ -1,3 +1,8 @@
 module "ec2_instance" {
   source = "./modules/ec2_instance"
+  region_value = "us-east-1"
+  ami_value = "ami-05cf1e9f73fbad2e2" # replace this
+  instance_type_value = "t2.micro"
+  subnet_id_value = "subnet-0bf3025525a4dd3d8" # replace this
+  instance_name_value = "test-instance"
 }
