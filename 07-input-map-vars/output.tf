1b9e821b2dbfcd58af969580b957399d73abd186
venkat <devops.bheemireddy@gmail.com>
2026-05-12 17:11:37 +1000

    Commit

--- 07-input-map-vars/output.tf
+++ 07-input-map-vars/output.tf
@@ -1,11 +1,11 @@
 output "ov1" {
-  value = var.mv1[item1]
+  value = var.mv1["item1"]
 }
 
 output "ov2" {
-  value = var.mv1[item2]
+  value = var.mv1["item2"]
 }
 
 output "ov3" {
-  value = var.mv1[item3]
+  value = var.mv1["item3"]
 }
