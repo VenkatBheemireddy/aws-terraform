368840c482bf7073e3e3da26c13d7191a6763fdf
VenkatBheemireddy <venkat.bheemireddy@gmail.com>
2026-05-13 15:20:46 +1000

    Terraform commit

---
+++ 13-backend-lock/main.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "venkat-remote-state-s3-bucket" ### change this
+    key            = "venkat/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"   ### change this
+  }
+}
