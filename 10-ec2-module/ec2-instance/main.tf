d99514cba3ce7bb0ff09e30b0a80cb861d86f5b1
venkat <devops.bheemireddy@gmail.com>
2026-05-13 14:15:37 +1000

    Terraform commit

---
+++ 10-ec2-module/ec2-instance/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = var.region_value
+}
+
+resource "aws_instance" "example" {
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+  subnet_id = var.subnet_id_value
+  associate_public_ip_address = true
+
+  tags = {
+    Name = var.instance_name_value
+  }
+}
