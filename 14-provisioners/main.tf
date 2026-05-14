a163edf036b7fb06be192606854b6e0ec689bf73
VenkatBheemireddy <venkat.bheemireddy@gmail.com>
2026-05-14 19:42:17 +1000

    Terraform commit

---
+++ 14-provisioners/main.tf
@@ -0,0 +1,125 @@
+### Step-1 Define the AWS provider configuration.
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region.
+}
+
+
+### Step-2 CIDR block for vpc creation
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+
+### Step-3 Create aws key pair to access the ec2 instance
+resource "aws_key_pair" "example" {
+  key_name   = "terraform-demo-venkat"  # Replace with your desired key name
+  public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
+}
+
+
+### Step-4 Create vpc network
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+}
+
+
+### Step-5 Create public subnet
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+
+### Step-6 Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+
+### Step-7 Create Route Table for IG
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+
+### Step-8 Associate Route Table with subnet
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+### Step-9 Create Security Group
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
+
+  ### Inbound rule, opening port 80
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ### Inbound rule opening port 22
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ### Outbound rule
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-sg"
+  }
+}
+
+### Create ec2 instance
+resource "aws_instance" "server" {
+  ami                    = "ami-05cf1e9f73fbad2e2"  ### You need to change this
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    host        = self.public_ip
+  }
+
+  # File provisioner to copy a file from local to the remote EC2 instance
+  provisioner "file" {
+    source      = "app.py"  # Replace with the path to your local file
+    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from the remote instance'",
+      "sudo apt update -y",  # Update package lists (for ubuntu)
+      "sudo apt-get install -y python3-pip",  # Example package installation
+      "cd /home/ubuntu",
+      "sudo pip3 install flask",
+      "sudo python3 app.py &",
+    ]
+  }
+}
