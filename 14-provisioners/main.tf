b3f64af78687708ac212ce97473a29a5781d45c2
VenkatBheemireddy <venkat.bheemireddy@gmail.com>
2026-05-14 23:37:23 +1000

    Terraform commit

--- 14-provisioners/main.tf
+++ 14-provisioners/main.tf
@@ -122,9 +122,7 @@ resource "aws_instance" "server" {
       "sudo apt update -y",  # Update package lists (for ubuntu)
       "sudo apt-get install -y python3-pip",  # Example package installation
       "cd /home/ubuntu",
-      # "sudo pip3 install flask",
-      "python3 -m venv venv",
-      "venv/bin/pip install flask",
+      "sudo apt-install python3-flask",
       "sudo python3 app.py &",
     ]
   }
